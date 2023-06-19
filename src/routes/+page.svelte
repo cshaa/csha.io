31e023f8b4500c865d1f89e568219e38f342aec3
Michal Grňo <m93a.cz@gmail.com>
2023-06-19 17:53:35 +0200

    f

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <h1>csha's homepage</h1>
+<p>Hey this is the personal page of Michal Grňo.</p>
+<p>You might want to check out <a href="https://github.com/m93a">my GitHub</a>.</p>
 <p>
-	Hey this is the personal page of Michal Grňo. You might want to check out <a
-		href="https://github.com/m93a">my GitHub</a
-	>. If you want to buy a ticket for ArtyParty, go to the subdomain:
+	If you want to buy a ticket for ArtyParty, go to the subdomain:
 	<a href="https://artyparty.csha.io">artyparty.csha.io</a>
 </p>
