f4c245a36f02d5ae6ea4d344f0f447940714d3fa
Michal Grňo <m93a.cz@gmail.com>
2023-09-05 17:08:59 +0200

    feat: made it look EXTREMELY DOPE. just sayin'

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,34 @@
-<h1>csha's homepage</h1>
-<p>Hey this is the personal page of Michal Grňo.</p>
-<p>You might want to check out <a href="https://github.com/m93a">my GitHub</a>.</p>
-<p>
-	If you want to buy a ticket for ArtyParty, go to the subdomain:
-	<a href="https://artyparty.csha.io">artyparty.csha.io</a>
-</p>
+<script lang="ts">
+	import '@fontsource/inter';
+	import './style.css';
+</script>
+
+<main>
+	<h1>csha's den</h1>
+	<p>Hey, welcome to my personal page!</p>
+	<p>
+		I'm a freelance UX designer by day, a neurodivergent anarcho-feminist by night. Just kidding,
+		I'm neurodivergent af all the time. And since I usually only start working in the evening... you
+		get the idea.
+	</p>
+	<p>
+		Currently, I study humanities at <a href="https://fhs.cuni.cz/FHSENG-1.html">FHS</a>. Before
+		that, I studied physics at <a href="https://www.mff.cuni.cz/en">MFF</a> where I was interested
+		especially Lagrangian, Hamiltonian and quantum mechanics, group theory and differential
+		geometry. In my bachelor's thesis, which
+		<a href="https://github.com/m93a/thesis-magnetic-transport">you can read here</a>, I
+		analytically investigated the spectra (ie. allowed energies) of electrons in a homogeneous
+		magnetic field and some kind of electrostatic field.
+	</p>
+	<p>
+		You might want to check out <a href="https://github.com/m93a">my GitHub</a>.<br />
+		You <strong>MUST</strong> check out <a href="https://garden.csha.io">my digital garden</a>!
+	</p>
+
+	<figure>
+		<img src="/fuck_da_police.svg" alt="A black and white drawing of a police car in flames" />
+		<figcaption>
+			(Art by <a href="https://www.etsy.com/shop/RucheCollection">Brooke La Ruche</a>)
+		</figcaption>
+	</figure>
+</main>
