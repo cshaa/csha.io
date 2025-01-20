a9d5cf1edf075cf830f6dd16d3fb72693d5dac64
m93a <m93a.cz@gmail.com>
2025-01-20 20:29:27 +0100

    feat(root): update info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,9 +7,9 @@
 	<h1>csha's den</h1>
 	<p>Hey, welcome to my personal page!</p>
 	<p>
-		I'm a freelance UX designer by day, and a neurodivergent anarcho-feminist by night. Just kidding,
-		I'm neurodivergent af all the time. And since I usually only start working in the evening... you
-		get the idea.
+		I'm a freelance UX designer by day, and a neurodivergent anarcho-feminist by night. Just
+		kidding, I'm neurodivergent af all the time. And since I usually only start working in the
+		evening... you get the idea.
 	</p>
 	<p>
 		I studied physics at <a href="https://www.mff.cuni.cz/en">MFF</a> where I was especially
@@ -22,8 +22,8 @@
 		Now, I'm picking up gardening & permaculture and hoping to grow a food forest!
 	</p>
 	<p>
-		I'm a member of <a href="https://codektiv.dev/">Codektiv</a>, and aspiring to become
-		a member of <a href="https://druit.org/">Druit</a>, too.
+		I'm a member of <a href="https://codektiv.dev/">Codektiv</a> and
+		<a href="https://druit.org/">Druit</a>.
 	</p>
 	<p>
 		You might want to check out <a href="https://github.com/cshaa">my GitHub</a> or
